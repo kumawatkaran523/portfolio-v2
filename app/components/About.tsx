e01f33613f21a11a0ff7b6d3f83e9fed5f679b55
ganeshkondaka <kondakaganesh2003@outlook.com>
2025-03-06 22:16:30 +0530

    added a web 3 in about

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -6,7 +6,7 @@ export default function About() {
             <h2 className='text-xl font-bold'>About Me</h2>
             <p className='text-xs sm:text-sm p-1 text-zinc-400'>Hey there! I'm Ganesh, a passionate full-stack developer who loves building sleek, functional, and creative web applications. I specialize in the <strong className='text-white'> MERN stack, Next.js, and TypeScript,</strong> with a strong focus on DevOps, <strong  className='text-white'> AI-powered tools, and open-source contributions</strong>.
                 I enjoy working on <strong> cutting-edge projects,</strong> from <strong className='text-white'> AI-driven content generation</strong> platforms to fun and useful npm libraries. My journey includes <strong className='text-white'> contributing to open-source, exploring Docker</strong>, and pushing the limits of modern web development.
-                When I'm not coding, you'll find me brainstorming wild project ideas, refining my DevOps skills, or diving into regional Indian accents just for fun!
+                When I'm not coding, you'll find me brainstorming wild project ideas, refining my DevOps skills, or diving into regional Indian accents just for fun!<strong className='text-white'> also started learning WEB 3</strong>
             </p>
         </div>
     )
