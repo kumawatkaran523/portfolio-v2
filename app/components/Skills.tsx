04df2b12608c9c3672f40af30aa7cb07d793bd48
ganeshkondaka <kondakaganesh2003@outlook.com>
2025-03-13 22:52:49 +0530

    added redis

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 export default function Skills() {
 
-    const skills_list:string[]=["Nextjs","Typescript","React","Expressjs","Prisma-ORM","TailwindCSS","MongoDB","Nodejs","Docker","Javascript","Python","Git","Machine learning","Tensorflow","Pandas","Keras"]
+    const skills_list: string[] = ["Nextjs", "Typescript", "React", "Docker", "TailwindCSS", "Prisma-ORM", "Expressjs", "MongoDB", "Nodejs", "Redis", "Javascript", "Python", "Git", "Machine learning", "Tensorflow", "Pandas", "Keras"]
 
-  return (
-    <div className='inline px-4 py-2'>
-        <p className='text-xl font-bold my-2'>Skills</p>
-        {
-            skills_list.map((item,index)=>(
-                <div key={index} className=" items-center inline-flex text-[12px] text-black font-bold  px-2 py-1 border-2 bg-white rounded-lg hover:bg-zinc-400 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
-                    {item}
-                </div>
-            ))
-        }
-    </div>
-  )
+    return (
+        <div className='inline px-4 py-2'>
+            <p className='text-xl font-bold my-2'>Skills</p>
+            {
+                skills_list.map((item, index) => (
+                    <div key={index} className=" items-center inline-flex text-[12px] text-black font-bold  px-2 py-1 border-2 bg-white rounded-lg hover:bg-zinc-400 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+                        {item}
+                    </div>
+                ))
+            }
+        </div>
+    )
 }
