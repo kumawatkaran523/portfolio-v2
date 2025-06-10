316558207676484c12035a6e9c5027e70fbd4b06
🅚🅐🅡🅐🅝 <kumawatkaran523@gmail.com>
2025-06-10 18:49:39 +0530

    moved app

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -11,6 +11,12 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+export const metadata = {
+  title: 'pixelpenguin.sh',
+  description: 'Ideas in pixels, stories in code.',
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
